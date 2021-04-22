579e8495f15403ae3987aaed470ac2639751a87f
solnine525 <solnine525@gmail.com>
2021-04-22 10:19:29 +0900

    집에서 작업끝

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	
 	public static void main(String[] args) {
-		
+		//깃이랑 연결되었네
 	}
 }
--- src/chat7/Receiver.java
+++ src/chat7/Receiver.java
@@ -8,7 +8,7 @@ import java.net.SocketException;
 
 //서버가 보내는 Echo 메세지를 읽어오는 쓰레드 클래스
 public class Receiver extends Thread {
-	
+	//~
 	Socket socket;
 	BufferedReader in = null;
 	
